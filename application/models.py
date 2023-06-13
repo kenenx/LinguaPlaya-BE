986f869bdab6c6baac2a8fdd0b4b6f2d5f749ec1
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-06-13 17:09:42 +0100

    auth basically kinda working

--- application/models.py
+++ application/models.py
@@ -1,103 +1,165 @@
 
-import jwt
+
+from application import app,db
+
+from passlib.hash import pbkdf2_sha256 as sha256
+
+app.app_context().push()
+db.create_all()
+
+class UserModel(db.Model):
+    """
+    User Model Class
+    """
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(120), unique=True, nullable=False)
+
+    password = db.Column(db.String(120), nullable=False)
+
+    """
+    Save user details in Database
+    """
+    def save_to_db(self):
+
+        db.session.add(self)
+
+        db.session.commit()
+
+    """
+    Find user by username
+    """
+    @classmethod
+    def find_by_username(cls, username):
+        
+        return cls.query.filter_by(username=username).first()
+
+    """
+    return all the user data in json form available in DB
+    """
+    @classmethod
+    def return_all(cls):
+        
+        def to_json(x):
+        
+            return {
+        
+                'username': x.username,
+        
+                'password': x.password
+        
+            }
+        
+        return {'users': [to_json(user) for user in UserModel.query.all()]}
+
+    """
+    Delete user data
+    """
+    @classmethod
+    def delete_all(cls):
+        
+        try:
+        
+            num_rows_deleted = db.session.query(cls).delete()
+        
+            db.session.commit()
+        
+            return {'message': f'{num_rows_deleted} row(s) deleted'}
+        
+        except:
+        
+            return {'message': 'Something went wrong'}
+
+    """
+    generate hash from password by encryption using sha256
+    """
+    @staticmethod
+    def generate_hash(password):
+        
+        return sha256.hash(password)
+
+    """
+    Verify hash and password
+    """
+    @staticmethod
+    def verify_hash(password, hash_):
+        
+        return sha256.verify(password, hash_)
+
+
+class RevokedTokenModel(db.Model):
+    """
+    Revoked Token Model Class
+    """
+
+    __tablename__ = 'revoked_tokens'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    
+    jti = db.Column(db.String(120))
+
+    """
+    Save Token in DB
+    """
+    def add(self):
+    
+        db.session.add(self)
+    
+        db.session.commit()
+
+    """
+    Checking that token is blacklisted
+    """
+    @classmethod
+    def is_jti_blacklisted(cls, jti):
+    
+        query = cls.query.filter_by(jti=jti).first()
+    
+        return bool(query)
+
+
+##############################################################
 import datetime
-from application import app,db, bcrypt
+from application import app,db
 
 app.app_context().push()
 db.create_all()
 
 # user models 
-class Users(db.Model):
-    users_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    username =  db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    registered_on = db.Column(db.DateTime, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    flags = db.Column(db.Integer, nullable=False)
-    # not sure about these
-    # connections = db.Column(db.Integer, foreign_key = True)
-    time_zone = db.Column(db.Integer, nullable=False)
-    last_online = db.Column(db.Integer, nullable=False)
+# class Users(db.Model):
+#     users_id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     username =  db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(100), nullable=False)
+#     password = db.Column(db.String(255), nullable=False)
+#     registered_on = db.Column(db.DateTime, nullable=False)
+#     # rating = db.Column(db.Integer, nullable=False, default = 5)
+#     # flags = db.Column(db.Integer, nullable=False, default = 0)
+#     # not sure about these
+#     # connections = db.Column(db.Integer, foreign_key = True)
+#     # time_zone = db.Column(db.Integer, nullable=False)
+#     # last_online = db.Column(db.Integer, nullable=False)
+
+# #rating,flags, connections, time_zone, last_online
+#     def __init__(self, name, email, username, password ):
+#         self.name = name
+#         self.email = email
+#         self.username = username
+#         self.password = bcrypt.generate_password_hash(
+#             password, app.config.get('BCRYPT_LOG_ROUNDS')
+#         ).decode()
+#         # self.rating = rating
+#         # self.flags = flags
+#         # self.connections = connections
+#         # self.time_zone = time_zone
+#         # self.last_online = last_online
+#         self.registered_on = datetime.datetime.now()
+    
 
 
-    def __init__(self, name, age, email, username, password, rating,flags, connections, time_zone, last_online):
-        self.name = name
-        self.age = age
-        self.email = email
-        self.username = username
-        self.password = bcrypt.generate_password_hash(
-            password, app.config.get('BCRYPT_LOG_ROUNDS')
-        ).decode()
-        self.rating = rating
-        self.flags = flags
-        self.connections = connections
-        self.time_zone = time_zone
-        self.last_online = last_online
-        self.registered_on = datetime.datetime.now()
-    
-    # Generates the Auth Token
-    def encode_auth_token(self, users_id):
-       
-        try:
-            payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': users_id
-            }
-            return jwt.encode(
-                payload,
-                app.config.get('SECRET_KEY'),
-                algorithm='HS256'
-            )
-        except Exception as e:
-            return e
-    
-    #Validates the auth token    
-    @staticmethod
-    def decode_auth_token(auth_token):
-       
-        try:
-            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
-            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
-            if is_blacklisted_token:
-                return 'Token blacklisted. Please log in again.'
-            else:
-                return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
-        except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
-        
 with app.app_context():
     db.create_all()
     print("Database tables created")
-
-# Token Model for storing JWT tokens
-class BlacklistToken(db.Model):
-
-    __tablename__ = 'blacklist_tokens'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    token = db.Column(db.String(500), unique=True, nullable=False)
-    blacklisted_on = db.Column(db.DateTime, nullable=False)
-
-    def __init__(self, token):
-        self.token = token
-        self.blacklisted_on = datetime.datetime.now()
-
-    def __repr__(self):
-        return '<id: token: {}'.format(self.token)
-    
-    # check whether auth token has been blacklisted
-    @staticmethod
-    def check_blacklist(auth_token):
-        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
-        if res:
-            return True
-        else:
-            return False
-
-
