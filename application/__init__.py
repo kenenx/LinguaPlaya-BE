a3f6833ebcf208ee730607e0498e857e92c7db79
kenenx <kenen1996@hotmail.co.uk>
2023-06-19 15:18:58 +0100

    add auth to cors header

--- application/__init__.py
+++ application/__init__.py
@@ -16,7 +16,7 @@ CORS(app)
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
 
 # Application Configuration
